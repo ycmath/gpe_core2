a7bed85fd5e80e004514a4337df897466d765a89
ycmath <30497102+ycmath@users.noreply.github.com>
2025-06-26 23:18:54 +0900

    Delete gpe_core directory

--- gpe_core/gpu/__init__.py
+++
@@ -1,9 +0,0 @@
-"""GPU accelerated components for GPE."""
-
-# GPU 모듈은 cupy가 설치된 경우에만 import
-try:
-    from .stream_decoder import GPEDecoderGPUStream
-    from .stream_decoder_meta import GPEDecoderGPUStreamFull
-    __all__ = ["GPEDecoderGPUStream", "GPEDecoderGPUStreamFull"]
-except ImportError:
-    __all__ = []
